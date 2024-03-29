9664d575017a2213c617c6bf0c2eb8f369261f08
Radcool510 <85513425+radcool510@users.noreply.github.com>
2024-03-29 22:47:47 +0200

    Update run.sh

--- bots/run.sh
+++ bots/run.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/env bash
+
+# Change directory to the location of the script
+cd "$(dirname "$0")"
+
 # Install the required packages from the requirements.txt file
 pip install -r requirements.txt
+
 # Create the condition file
 touch condition
+
 # Run the script as long as the condition file exists
-while [ -e condition ]
-do
+while [ -e condition ]; do
     python3 main.py
     git pull origin main
 done
