d9cd5b6b1caf2c054d5898416013f982d626e1ca
Radcool510 <85513425+radcool510@users.noreply.github.com>
2024-03-29 23:24:17 +0200

    Update run.sh

--- bots/run.sh
+++ bots/run.sh
@@ -1,22 +1,11 @@
 #!/usr/bin/env bash
-
-# Print the current directory
-echo "Current directory: $(pwd)"
-
-# Change directory to the location of the script
-cd "$(dirname "$0")"
-
-# Print the contents of the current directory
-echo "Contents of the directory: $(ls)"
-
 # Install the required packages from the requirements.txt file
 pip install -r requirements.txt
-
 # Create the condition file
 touch condition
-
 # Run the script as long as the condition file exists
-while [ -e condition ]; do
+while [ -e condition ]
+do
     python3 main.py
     git pull origin main
 done
