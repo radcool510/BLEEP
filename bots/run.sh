909ab558e1b03ccb8e1fafa5da75627bf248c103
Radcool510 <85513425+radcool510@users.noreply.github.com>
2024-03-29 22:12:36 +0200

    Update run.sh

--- bots/run.sh
+++ bots/run.sh
@@ -1,1 +1,11 @@
-
+#!/usr/bin/env bash
+# Install the required packages from the requirements.txt file
+pip install -r requirements.txt
+# Create the condition file
+touch condition
+# Run the script as long as the condition file exists
+while [ -e condition ]
+do
+    python3 main.py
+    git pull origin main
+done
